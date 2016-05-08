069ed79a0584962a41a9551faa47e8f8614e0f7f
unknown <albertzhu.7@gmail.com>
2016-05-08 16:01:23 -0400

    add updates.js and link in index.html

---
+++ javascripts/updates.js
@@ -0,0 +1,7 @@
+var updates = '{"updates":['+
+  '{"date":"5/8/2016","message":"HELLO WORLD"},'+
+  '{"date":"5/15/2016","message":"HELLO PEOPLE"},'+
+  '{"date":"5/22/2016","message":"KONICHIWA"}'+
+']}'
+
+alrt('updates.js');
