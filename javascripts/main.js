b5b8ddb586bc3ccde134f0a2dc7aab3500fcd233
unknown <albertzhu.7@gmail.com>
2016-05-22 15:11:58 -0400

    Refactored main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,12 @@
-//
-//
-// var updateWindow = document.getElementById('updateWindow');
-//
-// var updates = "New Update";
-//
-// updateWindow.innerHTML = updates;
+
+
+  // alert(obj);
+  var title = '<h1>New Updates</h1>';
+  var updateWindow = document.getElementById('updateWindow');
+
+updateWindow.innerHTML = title + "<br><br>";
+
+ // alert(obj.updates);
+ newUpdate.updates.forEach(function (update){
+    updateWindow.innerHTML = updateWindow.innerHTML + update.date + "  :  " + update.message + "<br><br>";
+ });
