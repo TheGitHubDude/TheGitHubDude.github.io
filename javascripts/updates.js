c1030bdc56af8171e8a934bfb364fc1a50d47c67
unknown <albertzhu.7@gmail.com>
2016-07-03 14:46:56 -0400

    updated updates

--- javascripts/updates.js
+++ javascripts/updates.js
@@ -1,7 +1,5 @@
 var newUpdate = {
   "updates":[
-    {"date":'5/26/2016',"message":'In Session'},
-    {"date":'6/02/2016',"message":'In Session'},
-    {"date":'6/09/2016',"message":'In Session'},
+    {"date":'6/10/2016',"message":'Last Day; Have a Happy Summer!'},
   ]
 }
