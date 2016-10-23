3e8a62d9f9e568286a66fb447e0be67b3c30da00
Albert Zhu <albertzhu.7@gmail.com>
2016-10-23 14:52:32 -0300

    Added Tournament Flyer PDF

--- javascripts/updates.js
+++ javascripts/updates.js
@@ -7,6 +7,6 @@ var newUpdate = {
     {
     	"date":'11/11/16, 10 AM - 4 PM',
     	"message":'Interscholastic Tournament <br> <b>Registration Deadline: 11/5/16; Cost: $10</b>'
-    },
+    }
   ]
 }
